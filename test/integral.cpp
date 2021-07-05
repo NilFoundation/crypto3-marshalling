ae2e397f2623f5c038109ddb6751ce4923a51932
nkaskov <mail.nikita.kaskov@gmail.com>
2021-07-05 13:47:28 +0300

    Initial multiprecision number marshalling type added #1

---
+++ test/integral.cpp
@@ -0,0 +1,49 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2018-2021 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2020-2021 Nikita Kaskov <nbering@nil.foundation>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//---------------------------------------------------------------------------//
+
+#define BOOST_TEST_MODULE crypto3_marshalling_integral_test
+
+#include <boost/array.hpp>
+#include <boost/cstdint.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#include <nil/marshalling/algorithms/pack.hpp>
+#include <nil/crypto3/marshalling/types/integral.hpp>
+
+#include <cstdio>
+
+using namespace nil::marshalling;
+using namespace nil::crypto3::marshalling;
+
+BOOST_AUTO_TEST_SUITE(pack_imploder_test_suite)
+
+BOOST_AUTO_TEST_CASE(be_to_be_1) {
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END()
